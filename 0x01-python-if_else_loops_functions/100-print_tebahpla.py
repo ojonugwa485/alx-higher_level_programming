7e0ad5b37ac1e17a9a9da68062f29731f735e3be
ojonugwa485 <jefferyegbunu@gmail.com>
2022-06-01 09:32:45 -0700

    loop function

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    c = i + ord('A')
-    if i % 2 == 1:
-        c += 32
-    print("{:c}".format(c), end="")
+for index in range(0, 26):
+    word = ord('z') - index
+    if (index % 2 == 1):
+        word = chr(word - ord('a') + ord('A'))
+    else:
+        word = chr(word)
+    print("{}".format(word), end='')
