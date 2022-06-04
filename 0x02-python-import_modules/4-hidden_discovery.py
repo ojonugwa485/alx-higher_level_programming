3c4ecaddfb440e1406b5524c28f8fccd06903810
ojonugwa485 <jefferyegbunu@gmail.com>
2022-06-04 02:39:54 -0700

    import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    arr = dir(hidden_4)
-    for i in range(len(arr)):
-       if (arr[i][:2] != "__"):
-           print(arr[i])
+    for l in dir(hidden_4):
+        if l[0:2] != "__":
+           print(l)
