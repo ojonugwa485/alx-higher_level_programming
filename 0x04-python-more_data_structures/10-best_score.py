7c88c1a07b9d1610d31995e3148759f0ab3c3263
ojonugwa485 <jefferyegbunu@gmail.com>
2022-06-10 06:15:36 -0700

    jeffrey

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -10,6 +10,6 @@ def best_score(a_dictionary):
      best_score = 0
      for key, value in a_dictionary.items():
          if value > best_score:
-            best_score = value
-            best_key = key
+          best_score = value
+          best_key = key
      return best_key
