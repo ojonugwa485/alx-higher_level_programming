6ecdbaf7520a1d369717c9f556c437e7dd3b1742
ojonugwa485 <jefferyegbunu@gmail.com>
2022-06-10 05:51:43 -0700

    more data structure

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    """
-    Delete a key in a dictionary
-    """
-    if key in a_dictionary:
-       del a_dictionary[key]
+    if a_dictionary.get(key) is not None:
+        del a_dictionary[key]
     return a_dictionary
