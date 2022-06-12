4fa44538d20a2334ab2eb55138dd2650cbf4cfc6
ojonugwa485 <jefferyegbunu@gmail.com>
2022-06-12 01:26:11 -0700

    more data structure

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
-       return 0
+      return 0
     total = 0
     num = 0
     digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
